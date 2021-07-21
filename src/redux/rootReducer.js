d3d6c49e8e6fdf00d172130a8b34415bf39c6d61
haonguyen0110 <71426649+haonguyen0110@users.noreply.github.com>
2021-07-21 13:25:19 +0700

    Update rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -6,16 +6,6 @@ const rootReducer = combineReducers({
 });
 
 
-//Đối với state là object hoặc array
-// => Redux chỉ render lại khi object hoặc array được gắn object hoặc array với imutable
-/*
-    Ví dụ có objectA
-    objectA.thuoctinh= giá trị mới => không thay đổi (redux không biết đc giá trị đó thay đổi =>k render lại giao diên)
-
-    objectA={} =>gán = object mới
-    objectA={...objectA, thuocTinh:'giaTriMoi'} 
-
-*/
 
 export const store = createStore(rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
